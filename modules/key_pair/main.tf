33ccd417709c8c1d478704832bb638b084ae8ddf
Ferhan-J-Patel <ferhan.patel@operisoft.com>
2023-03-15 16:45:45 +0530

    Pem file

--- modules/key_pair/main.tf
+++ modules/key_pair/main.tf
@@ -1,4 +1,9 @@
 resource "aws_key_pair" "example" {
   key_name   = var.key_pair_name
-  public_key = file("home/fjp/example-key.pub")
+  public_key = tls_private_key.example.public_key_openssh
+}
+
+resource "local_file" "public_key" {
+  content  = aws_key_pair.example.public_key
+  filename = "/home/ubuntu/example-key.pub"
 }
