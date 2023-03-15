04aafbdba4ee8d6ac17e044ff6c456061ef64cd8
Ferhan-J-Patel <ferhan.patel@operisoft.com>
2023-03-15 16:47:11 +0530

    ...

--- modules/key_pair/main.tf
+++ modules/key_pair/main.tf
@@ -1,3 +1,8 @@
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
 resource "aws_key_pair" "example" {
   key_name   = var.key_pair_name
   public_key = tls_private_key.example.public_key_openssh
