16e098ecbc3d7a5188c9223fdbae6ca9d66d9305
Ferhan-J-Patel <ferhan.patel@operisoft.com>
2023-03-15 16:32:35 +0530

    RSA added

--- modules/key_pair/main.tf
+++ modules/key_pair/main.tf
@@ -1,3 +1,8 @@
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
 resource "local_file" "private_key" {
   content  = tls_private_key.example.private_key_pem
   filename = "${home_dir}/example-key.pem"
