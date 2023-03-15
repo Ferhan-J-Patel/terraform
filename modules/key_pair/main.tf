3159fce0321d3b260fca895cc25d6d58bd6666c4
Ferhan-J-Patel <ferhan.patel@operisoft.com>
2023-03-15 16:34:38 +0530

    Saving pem key in home user dir

--- modules/key_pair/main.tf
+++ modules/key_pair/main.tf
@@ -5,5 +5,5 @@ resource "tls_private_key" "example" {
 
 resource "local_file" "private_key" {
   content  = tls_private_key.example.private_key_pem
-  filename = "${home_dir}/example-key.pem"
+  filename = "home/fjp/example-key.pem"
 }
