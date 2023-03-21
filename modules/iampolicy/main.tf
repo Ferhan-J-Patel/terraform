f9627f50a6595a5609e91007fcd863a6f09db07f
FJP10 <ferhan.patel10@gmail.com>
2023-03-21 20:20:13 +0530

    ...

--- modules/iampolicy/main.tf
+++ modules/iampolicy/main.tf
@@ -1,14 +1,18 @@
+resource "aws_iam_policy" "custom_policy" {
+  name        = "AmazonEC2FullAccess"
+  description = "A ec2 policy"
+  policy      = "${file("region_restrict.json")}"
+}
+
+output "custom_policy_arn" {
+  value = aws_iam_policy.custom_policy.arn
+}
+
 variable "policy_arns" {
     type = list(string)
     default = ["arn:aws:iam::aws:policy/AmazonEC2FullAccess",
                 "${aws_iam_policy.custom_policy.0.arn}"
                 ]
-}
-
-resource "aws_iam_policy" "custom_policy" {
-  name        = "AmazonEC2FullAccess"
-  description = "A ec2 policy"
-  policy      = "${file("region_restrict.json")}"
 }
 
 resource "aws_iam_user_policy_attachment" "demo-attach"{
