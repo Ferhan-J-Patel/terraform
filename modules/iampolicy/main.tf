9d2ca0853cd0f0bb48d8eb5353154834aa47a89c
FJP10 <ferhan.patel10@gmail.com>
2023-03-21 19:43:52 +0530

    ...

--- modules/iampolicy/main.tf
+++ modules/iampolicy/main.tf
@@ -4,7 +4,13 @@ resource "aws_iam_policy" "policy" {
   policy      = "${file("region_restrict.json")}"
 }
 
+variable "policy_arns" {
+    type = list(string)
+    value = ["arn:aws:iam::aws:policy/AmazonEC2FullAccess","aws_iam_policy.policy.arn"]
+}
+
 resource "aws_iam_user_policy_attachment" "demo-attach"{
+    count = length(var.policy_arns)
     user = var.iam_user
-    policy_arn = ["arn:aws:iam::aws:policy/AmazonEC2FullAccess","aws_iam_policy.policy.0.arn"]
+    policy_arn = var.policy_arns[count.index].value
 }
