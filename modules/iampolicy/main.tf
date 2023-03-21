0f55d4e73f302deea0374de5930ed7e0f5a704da
FJP10 <ferhan.patel10@gmail.com>
2023-03-21 19:07:13 +0530

    multiple policies attachment

--- modules/iampolicy/main.tf
+++ modules/iampolicy/main.tf
@@ -1,10 +1,10 @@
-# resource "aws_iam_policy" "policy" {
-#   name        = "AmazonEC2FullAccess"
-#   description = "A ec2 policy"
-#   policy      = "${file("ec2fullaccess.json")}"
-# }
+resource "aws_iam_policy" "policy" {
+  name        = "AmazonEC2FullAccess"
+  description = "A ec2 policy"
+  policy      = "${file("region_restrict.json")}"
+}
 
 resource "aws_iam_user_policy_attachment" "demo-attach"{
     user = var.iam_user
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+    policy_arn = ["arn:aws:iam::aws:policy/AmazonEC2FullAccess","aws_iam_policy.policy.arn"]
 }
