e4a5a90781df94c1ebb5b25fe24bfb585a13f51e
FJP10 <ferhan.patel10@gmail.com>
2023-03-21 20:49:50 +0530

    ...

--- modules/iampolicy/main.tf
+++ modules/iampolicy/main.tf
@@ -4,9 +4,13 @@ resource "aws_iam_policy" "custom_policy" {
   policy      = "${file("region_restrict.json")}"
 }
 
+output "custom_policy_arn" {
+    value = aws_iam_policy.custom_policy.arn
+}
+
 resource "aws_iam_user_policy_attachment" "demo-attach"{
     for_each = toset(["arn:aws:iam::aws:policy/AmazonEC2FullAccess",
-                "${aws_iam_policy.custom_policy_arn}"
+                aws_iam_policy.custom_policy.arn
                 ])
 
     user = var.iam_user
