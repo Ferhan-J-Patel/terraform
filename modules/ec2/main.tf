e11dad6a57632ff83cfbb9348d5a287c97251b44
Ferhan-J-Patel <126552299+Ferhan-J-Patel@users.noreply.github.com>
2023-07-26 18:04:23 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -7,4 +7,13 @@ resource "aws_instance" "main" {
   vpc_security_group_ids = var.sg_ids
 
   tags = var.tags
+
+  user_data = <<EOF
+  #!/bin/bash
+  sudo ./xfce.sh
+  sudo apt install -y dbus-x11
+  sudo systemctl enable xrdp --now
+  sudo echo kali:kali | sudo chpasswd
+  sudo reboot
+  EOF
 }
